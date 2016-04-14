43fade18afa46f2187e809150f4af4d8b8441f8e
Alexander Kurtakov <akurtako@redhat.com>
2016-04-14 12:34:37 +0300

    Generify IFormatterCommentableNode.
    Method insertBefore accepts only List<? extends IFormatterNode>.
    
    Change-Id: Ie36768d37a21b660823e20f667cd616aaeb31c65
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- core/plugins/org.eclipse.dltk.formatter/src/org/eclipse/dltk/formatter/IFormatterCommentableNode.java
+++ core/plugins/org.eclipse.dltk.formatter/src/org/eclipse/dltk/formatter/IFormatterCommentableNode.java
@@ -1,9 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2016 xored software, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     xored software, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.dltk.formatter;
 
 import java.util.List;
 
 public interface IFormatterCommentableNode {
 
-	void insertBefore(List comments);
+	void insertBefore(List<? extends IFormatterNode> comments);
 
 }
