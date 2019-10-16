502259e6cf1140e363231d8b0365940793f20d61
Alexander Kurtakov <akurtako@redhat.com>
2019-10-16 18:03:34 +0300

    Switch to varargs TreeVeiewer.add method.
    After platform change.
    
    Change-Id: I4648b2842dbee3e5e83efd8d789c8398d0900e68
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/scriptview/ScriptExplorerPart.java
+++ core/plugins/org.eclipse.dltk.ui/src/org/eclipse/dltk/internal/ui/scriptview/ScriptExplorerPart.java
@@ -3,7 +3,7 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  *******************************************************************************/
@@ -228,11 +228,11 @@ public class ScriptExplorerPart extends ViewPart implements ISetSelectionTarget,
 
 		public PackageExplorerProblemTreeViewer(Composite parent, int style) {
 			super(parent, style);
-			fPendingRefreshes = Collections.synchronizedList(new ArrayList());
+			fPendingRefreshes = Collections.synchronizedList(new ArrayList<>());
 		}
 
 		@Override
-		public void add(Object parentElement, Object[] childElements) {
+		public void add(Object parentElement, Object... childElements) {
 			if (fPendingRefreshes.contains(parentElement)) {
 				return;
 			}
